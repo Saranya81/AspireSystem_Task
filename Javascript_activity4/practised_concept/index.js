92e99117a228eeb986b0f9bfe81419305e4cecb1
SARANYA R <124616598+Saranya81@users.noreply.github.com>
2024-04-27 08:09:08 +0530

    Add files via upload

---
+++ Javascript_activity4/practised_concept/index.js
@@ -0,0 +1,61 @@
+ 
+//  // title : Javascript functions
+// //Author :Saranya
+// //Create date:27-03-2024
+
+// Array of transactions
+const transactions = [
+    { amount: 100, accountNumber: "1234567890" },
+    { amount: 200, accountNumber: "0987654321" },
+    { amount: 50, accountNumber: "1357902468" }
+];
+
+function handleTransactions(transactions) {
+    // Map function 
+    const processedTransactions = transactions.map(transaction => {
+        console.log(`Transaction of $${transaction.amount} initiated for account ${transaction.accountNumber}`);
+    
+    });
+
+    // Array length
+    console.log("Number of transactions:", transactions.length);
+
+    // Find function
+    const accountNumberToFind = "0987654321";
+    const foundTransaction = transactions.find(transaction => transaction.accountNumber === accountNumberToFind);
+
+    if (foundTransaction) {
+        console.log("Transaction found ");
+    } else {
+        console.log("Transaction not found");
+    }
+
+
+    // Filter
+    const highAmountTransactions = transactions.filter(transaction => transaction.amount > 100);
+    console.log("Transactions with amount greater than $100:", highAmountTransactions);
+
+    // Reversing  
+    const reversedTransactions = transactions.slice().reverse();
+    console.log("Reversed transactions:", reversedTransactions);
+}
+
+handleTransactions(transactions);
+
+
+// Callback function 
+function performBankTransaction(amount, callback) {
+    const success = amount > 0;
+    setTimeout(() => {
+        callback(success ? "success" : "failure");
+    }, 2000); 
+}
+
+function transactionCallback(result) {
+    if (result === "success") {
+        console.log("Transaction successful!");
+    } else {
+        console.log("Transaction failed. Please try again later.");
+    }
+}
+performBankTransaction(100, transactionCallback);
