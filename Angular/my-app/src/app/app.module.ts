53ba6bfecb9808e832e11fe34579fdc0b00ade0f
SARANYA R <124616598+Saranya81@users.noreply.github.com>
2024-05-22 09:20:02 +0530

    Add files via upload

---
+++ Angular/my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common'; 
+import { FormsModule } from '@angular/forms';
+import { AccountComponent } from './account/account.component';
+import { RouterLink } from '@angular/router';
+import { CubePipe } from './cube.pipe';
+import { ProductService } from './product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccountComponent
+      //all components are listed here
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    //modules are listed here
+  ],
+  providers: [ProductService],//services are listed here
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+ }
